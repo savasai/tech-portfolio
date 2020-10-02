c9b2a1f29d4f9b3fe2dbdbe5307bd98415885a99
savasai <sailaja.savanam@gmail.com>
2020-10-02 14:19:47 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,7 +15,7 @@ const siteConfig = {
     title: 'Sailaja Savanam', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://savasai.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
-        twitter: '',
-        github: '',
+        linkedin: 'sailaja-savanam-3135083',
+        twitter: 'sailajaraman',
+        github: 'savasai',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
